1289d3887e1ae33ecb1a9c858338c79f2c207252
Jaxon Adams <jadamsresume1@gmail.com>
2022-05-12 13:39:23 -0600

    initial setup and server connection

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Set up an instance of express
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Set up mongo connection
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Log mongo queries
+mongoose.set('debug', true);
+
+app.listen(() => console.log(`Listening on port ${PORT}`));
